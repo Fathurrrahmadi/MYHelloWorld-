07eb75680c0fc534105c50adfb9f8258bbd28721
Fathur Rizki Rahmadi <fathurriskirahmadi@icloud.com>
2018-10-29 20:58:50 +0700

    Initial Commit

---
+++ HelloWorld!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld!
+//
+//  Created by Fathur Rizki Rahmadi on 29/10/18.
+//  Copyright © 2018 Fathur Rizki Rahmadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
